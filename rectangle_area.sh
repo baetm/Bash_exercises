8d493f5ae62add12a48794ac54ee8c1ba7c4a383
Bartosz Etmanski <bartosz.etmanski@gmail.com>
2022-09-20 20:06:03 +0200

    Add rectangle area computes script.

---
+++ rectangle_area.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Thsi script camputes area of rectangle
+
+read -p "Give first side of a rectangle: " a
+read -p "Give second side of the rectangle: " b
+
+echo "The area of the rectangle is equal:  $((a*b))"
